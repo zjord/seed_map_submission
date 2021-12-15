435164466698cc33c408149a7bc8874ee4a0d92b
Oscar Healy <oscarhealy3@gmail.com>
2021-12-15 18:57:56 +0000

    No more pubs

--- react-leaflet-map/src/App.js
+++ react-leaflet-map/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
 import { Icon } from "leaflet";
-import * as pubData from "./data/fulham_pubs.json"; //This is just an example of geojson data
 
 function App() {
 
@@ -12,12 +11,6 @@ function App() {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
-            {pubData.features.map(pub => (
-                <Marker
-                    key={pub.properties.fsa_ID}
-                    position={[pub.geometry.coordinates[1], pub.geometry.coordinates[0]]}
-                />
-            ))}
         </MapContainer>
     );
 
