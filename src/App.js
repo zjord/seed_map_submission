0358baf987095eb67c529c11e601af3fb2fcdf34
jrlove11 <jl3619@ic.ac.uk>
2022-01-05 18:51:54 +0000

    formatting title with preliminary description and instructions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React, {Component} from 'react'
 import Table from './Table'
 import Form from "./Form";
+import logo from "./assets/image.png"
+import description from "./assets/description.txt"
 
 //from oscar_dev
 // TODO investigate dependence on "public" folder (eg, index.html)
@@ -33,11 +35,19 @@ export default class App extends Component {
         console.log(this.state)
 
         return (
-            <div className="container">
-                <img src="logo1.png" alt={"logo"} width={'100'} loading={'eager'}/>
-                <h1 style={{textAlign: "center"}}>React-based User Input Webapps</h1>
-                <h4 style={{textAlign: "center"}}>Dandelion Seed Mapping Project</h4>
-                <p style={{textAlign: "center"}}>ZJORD, Imperial College 2021/22</p>
+            <div className="container" style={{"padding-top": '5em'}}>
+                <img className={'img inline'}  src={logo} alt={"logo"}/>
+                <h1 className={'small inline'}>Dandelion Seed Mapping Project</h1>
+                <p className={'inline'}>ZJORD, Imperial College 2021/22</p>
+                <div className={'horizontal'} style={{"margin-top":'1em',"margin-bottom":'1em'}}/>
+                <p style={{"margin-left":'0.5em', "font-weight":'bold'}}> ABOUT</p>
+                <p style={{"margin-left":'2em',"margin-right":'2em'}}> The goal of this website is to collect data from the general public about the location of dandelion seeds all released from our
+                    lab at Imperial College London.<br/><br/> Nature has invented ways many ways to fly however unlike the muscle based flight of animals, plants have developed
+                    clever structural designs to utilise wind. We want to track dandelion seeds by painting them a bright colour for you to find and input into this website.
+                </p>
+                <p style={{"margin-left":'0.5em', "font-weight":'bold'}}> INSTRUCTIONS</p>
+                <p style={{"margin-left":'2em',"margin-right":'2em'}}> If you have found a dandelion that is an unnatural colour, you have found one of the many dandelion seeds that we have released, use
+                this website to upload a picture of the seed along with the time and location when you found it.</p>
                 <p style={{textAlign: "left"}}>[Preview table]</p>
                 <Table
                     dData={dData}
